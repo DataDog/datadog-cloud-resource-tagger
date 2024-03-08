db1e10cea674aaf6489ce958ff9ad06a50fa2b0a
Bahar Shah <bahar.shah@datadoghq.com>
2024-03-08 17:04:04 -0500

    update tags

--- src/terraform/structure/main_resource_tagged.tf
+++ src/terraform/structure/main_resource_tagged.tf
@@ -3,6 +3,10 @@ resource aws_iam_role "iam_for_eks" {
   assume_role_policy = data.aws_iam_policy_document.iam_policy_eks.json
 
   tags = {
-    Team = "test"
+    Team                      = "test"
+    dd_git_file               = "src/terraform/structure/main_resource_tagged.tf"
+    dd_git_org                = "DataDog"
+    dd_git_repo               = "datadog-cloud-resource-tagger"
+    dd_git_resource_signature = "resource.aws_iam_role.iam_for_eks"
   }
 }
--- src/terraform/structure/main_tagged.tf
+++ src/terraform/structure/main_tagged.tf
@@ -7,5 +7,9 @@ module "complete_sg" {
   ingress_rules       = ["https-443-tcp"]
   tags = {
     dd_git_create_commit      = "6028228c8899b558e9e8e9c2dd29c6a05c3667a7"
+    dd_git_file               = "src/terraform/structure/main_tagged.tf"
+    dd_git_org                = "DataDog"
+    dd_git_repo               = "datadog-cloud-resource-tagger"
+    dd_git_resource_signature = "module.complete_sg"
   }
 }
