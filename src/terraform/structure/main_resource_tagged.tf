04cf4fa2c1dabb186729e6e0628610dbe5fd7174
Bahar Shah <bahar.shah@datadoghq.com>
2024-04-11 14:38:32 -0400

    Adding tags from datadog-cloud-resource-tagger

--- src/terraform/structure/main_resource_tagged.tf
+++ src/terraform/structure/main_resource_tagged.tf
@@ -4,5 +4,9 @@ resource aws_iam_role "iam_for_eks" {
 
   tags = {
     Team                      = "test"
+    dd_git_file               = "src/terraform/structure/main_resource_tagged.tf"
+    dd_git_org                = "DataDog"
+    dd_git_repo               = "datadog-cloud-resource-tagger"
+    dd_git_resource_signature = "resource.aws_iam_role.iam_for_eks"
   }
 }
